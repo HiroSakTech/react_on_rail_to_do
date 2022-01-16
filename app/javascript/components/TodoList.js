d0b9abf153ed38555d1e7a7b46aef3c7119302f8
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2022-01-16 15:03:21 +0900

    add todolist index view

--- app/javascript/components/TodoList.js
+++ app/javascript/components/TodoList.js
@@ -70,10 +70,95 @@ const EditButton = styled.span`
 `
 
 function TodoList() {
+  const [todos, setTodos] = useState([])
+  const [searchName, setSearchName] = useState('')
+
+  useEffect(() => {
+    axios.get('api/v1/todos.json')
+    .then(resp => {
+      console.log(resp.data);
+      setTodos(resp.data);
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }, [])
+
+  const removeAllTodos = () => {
+    const sure = window.confirm('Are you sure?');
+    if (sure) {
+      axios.delete('/api/v1/todos/destroy_all')
+      .then(resp => {
+        setTodos([])
+      })
+      .catch(e => {
+        console.log(e)
+      })
+    }
+  }
+
+  const updateIsCompleted = (index, val) => {
+    var data = {
+      id: val.id,
+      name: val.name,
+      is_completed: !val.is_completed
+    }
+    axios.patch(`/api/v1/todos/${val.id}`, data)
+    .then(resp => {
+      const newTodos = [...todos]
+      newTodos[index].is_completed = resp.data.is_completed
+      setTodos(newTodos)
+    })
+  }
+
   return (
-    <div>
-      TodoList
-    </div>
+    <>
+      <h1>TodoList</h1>
+      <SearchAndButton>
+        <SearchForm
+          type="text"
+          placeholder="Search todo..."
+          onChange={event => {
+            setSearchName(event.target.value)
+          }}
+        />
+        <RemoveAllButton onClick={removeAllTodos}>
+          RemoveAll
+        </RemoveAllButton>
+      </SearchAndButton>
+
+      <div>
+        {todos.filter((val) => {
+          if(searchName === "") {
+            return val
+          } else if (val.name.toLowerCase().includes(searchName.toLowerCase())) {
+            return val
+          }
+        }).map((val, key) => {
+          return (
+            <Row key={key}>
+              {val.is_completed ? (
+                <CheckedBox>
+                  <ImCheckboxChecked onClick={() => updateIsCompleted(key, val)} />
+                </CheckedBox>
+              ) : (
+                <UncheckedBox>
+                  <ImCheckboxUnchecked onClick={() => updateIsCompleted(key, val)} />
+                </UncheckedBox>
+              )}
+              <TodoName is_completed={val.is_completed}>
+                {val.name}
+              </TodoName>
+              <Link to={"/todos/" + val.id + "/edit"}>
+                <EditButton>
+                  <AiFillEdit />
+                </EditButton>
+              </Link>
+            </Row>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
